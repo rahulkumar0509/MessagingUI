aac05d48dfdac42fa5ec73d9c968ab7a4c7261e9
Rahul Kumar <rahulkumar0509@outlook.com>
2026-05-31 01:59:50 +0530

    correct tenant id and client id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,12 @@ import { PublicClientApplication, BrowserCacheLocation } from '@azure/msal-brows
 // Paste your actual IDs here
 const msalInstance = new PublicClientApplication({
   auth: {
-    clientId: 'a4dd4c5f-fda6-4bde-8f21-cbc02d0bf15f', 
-    authority: 'https://login.microsoftonline.com/66861b95-153a-4de5-9e9e-4f21625e3d01', 
+    clientId: 'fc530a19-8028-44b7-923a-d6c547712d02', 
+    authority: 'https://login.microsoftonline.com/a4dd4c5f-fda6-4bde-8f21-cbc02d0bf15f', 
     redirectUri: 'https://yellow-bay-0b3d1841e.7.azurestaticapps.net/' 
   },
   cache: {
     cacheLocation: BrowserCacheLocation.LocalStorage
-    
   }
 });
 
